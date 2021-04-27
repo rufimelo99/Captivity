057d7119ec34f8123a3d9bc03e58f578456930bc
eskapinakis <eduardoskapinakis1234@gmail.com>
2021-04-27 17:39:29 +0000

    first banana

---
+++ Prototype/Assets/PlayerMovement.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float moveSpeed = 5f;
+
+    public Rigidbody2D rb;
+    public Animator animator;
+    public int playerColor = 0;
+
+    Vector2 movement;
+
+
+    void Start()
+    {
+        animator.SetInteger("Color", playerColor);
+    }
+
+    //void OnTriggerEnter2D(Collider2D obj)
+    //{
+    //    if (obj.tag == "endGame")
+    //    {
+    //        Application.Quit();
+    //    }
+    //}
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        changeColor();
+
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+
+        animator.SetFloat("Horizontal", movement.x);
+        animator.SetFloat("Vertical", movement.y);
+        animator.SetFloat("Speed", movement.sqrMagnitude);
+    }
+
+
+    void changeColor()
+    {
+        if (Input.GetKeyDown(KeyCode.Alpha0))
+        {
+            playerColor = 0;
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            playerColor = 1;
+        }
+
+        animator.SetInteger("Color", playerColor);
+    }
+
+
+    void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
+        
+        if(Input.GetAxisRaw("Horizontal")!= 0 || Input.GetAxisRaw("Vertical") != 0)
+        {
+            animator.SetFloat("lastMoveX", Input.GetAxisRaw("Horizontal"));
+            animator.SetFloat("lastMoveY", Input.GetAxisRaw("Vertical"));
+        }
+    }
+}
---
+++ Prototype/Assets/Weapon.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Weapon : MonoBehaviour
+{
+
+    public Transform firePoint;
+    public GameObject bulletPrefab;
+    public GameObject projectile;
+    public int bulletColor = 0;
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        turnWeapon();
+        changeBulletColor();
+
+
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Shoot();
+        }
+    }
+
+    void changeBulletColor()
+    {
+        if (Input.GetKeyDown(KeyCode.Alpha0))
+        {
+            bulletColor = 0;
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            bulletColor = 1;
+        }
+    }
+
+    void Shoot()
+    {
+        projectile = Instantiate(bulletPrefab, firePoint.position, firePoint.rotation);
+        projectile.GetComponent<Bullet>().makeColor(bulletColor);
+    }
+
+
+    void turnWeapon()
+    {
+        if (Input.GetAxisRaw("Horizontal") == 1)
+        {
+            firePoint.rotation = Quaternion.Euler(0f, 0f, 0f);
+        }
+
+        if (Input.GetAxisRaw("Horizontal") == -1)
+        {
+            firePoint.rotation = Quaternion.Euler(0f, 180f, 0f);
+        }
+
+        if (Input.GetAxisRaw("Vertical") == 1)
+        {
+            firePoint.rotation = Quaternion.Euler(0f, 0f, 90f);
+        }
+
+        if (Input.GetAxisRaw("Vertical") == -1)
+        {
+            firePoint.rotation = Quaternion.Euler(0f, 0f, 270f);
+        }
+    }
+
+}
