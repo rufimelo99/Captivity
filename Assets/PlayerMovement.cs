5c9edf9000c483d62320bf5888939988d8ad30d7
Melo <rufimelo99@gmail.com>
2021-05-01 20:41:51 +0100

    Working Prototype kinda

---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,174 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField]
+    private int player;
+
+    public string PlayerColor = "Color";
+    public string PlayerHorizontal = "Horizontal";
+    public string PlayerVertical = "Vertical";
+
+
+    private KeyCode heroBlue = KeyCode.K;
+    private KeyCode heroGreen = KeyCode.L;
+    public KeyCode playerFire = KeyCode.Minus;
+    public KeyCode playerCombination = KeyCode.RightShift;
+    public bool tryingCombination = false;
+    public float moveSpeed = 5f;
+
+    public Rigidbody2D rb;
+    public Animator animator;
+    public int playerColor = 0;
+
+    Vector2 movement;
+    Vector2 roomMover;
+
+    public float health = 10f;
+    public Image healthBar;
+
+
+    void Start()
+    {
+        if (player == 2)
+        {
+            PlayerColor = "Color2";
+            PlayerHorizontal = "Horizontal2";
+            PlayerVertical = "Vertical2";
+
+
+            heroGreen = KeyCode.Q;
+            heroBlue = KeyCode.E;
+
+
+
+            playerFire = KeyCode.Space;
+            playerCombination = KeyCode.V;
+        }
+        animator.SetInteger(PlayerColor, playerColor);
+        healthBar.GetComponent<Image>().color = Color.green;
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        changeColor();
+        move();
+        animate();
+        if (health <= 0.1)
+        {
+            die();
+        }
+    }
+
+
+    void die()
+    {
+        health = 10f;
+        SceneManager.LoadSceneAsync(SceneManager.GetActiveScene().buildIndex);
+    }
+
+    void animate()
+    {
+        animator.SetFloat("Horizontal", movement.x);
+        animator.SetFloat("Vertical", movement.y);
+        animator.SetFloat("Speed", movement.sqrMagnitude);
+        healthBar.fillAmount = health / 10f;
+    }
+
+
+    void move()
+    {
+        movement.x = Input.GetAxisRaw(PlayerHorizontal);
+        movement.y = Input.GetAxisRaw(PlayerVertical);
+    }
+
+
+    void changeColor()
+    {
+        if (Input.GetKeyDown(heroGreen))
+        {
+            playerColor = 0;
+            healthBar.GetComponent<Image>().color = Color.green;
+        }
+        if (Input.GetKeyDown(heroBlue))
+        {
+            playerColor = 1;
+            healthBar.GetComponent<Image>().color = Color.blue;
+        }
+        animator.SetInteger(PlayerColor, playerColor);
+    }
+
+
+    void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
+
+        rotate();
+    }
+
+
+    void rotate()
+    {
+
+        if (Input.GetAxisRaw(PlayerHorizontal) != 0 || Input.GetAxisRaw(PlayerVertical) != 0)
+        {
+            animator.SetFloat("lastMoveX", Input.GetAxisRaw(PlayerHorizontal));
+            animator.SetFloat("lastMoveY", Input.GetAxisRaw(PlayerVertical));
+        }
+    }
+
+    void OnCollisionEnter2D(Collision2D obj)
+    {
+        if (obj.gameObject.tag == "Evil Touch")
+        {
+            health = health - 0.1f;
+        }
+    }
+
+    void OnCollisionStay2D(Collision2D obj)
+    {
+        if (obj.gameObject.tag == "Evil Touch")
+        {
+            health = health - 0.1f;
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+
+        if (col.tag == "Obstacle_blue" && playerColor != 1)
+        {
+            health = 0;
+        }
+
+        if (col.tag == "Vertical Room Changer")
+        {
+            roomMover = new Vector2(0, 2 * movement.y);
+            gameObject.transform.position = rb.position + roomMover;
+        }
+
+        if (col.tag == "Horizontal Room Changer")
+        {
+            roomMover = new Vector2(2 * movement.x, 0);
+            gameObject.transform.position = rb.position + roomMover;
+        }
+
+        if (col.tag == "Small Enemy Bullet")
+        {
+            health = health - 1;
+            healthBar.fillAmount = health / 10f;
+        }
+
+        if (col.tag == "endGame")
+        {
+            Application.Quit();
+        }
+    }
+
+}
---
+++ Assets/Weapon.cs
@@ -0,0 +1,98 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Weapon : MonoBehaviour
+{
+
+    [SerializeField]
+    private PlayerMovement player;
+    private int bulletColor = 0;
+
+
+    public Transform firePoint;
+    public GameObject bulletPrefab;
+
+    //TODO
+    public PlayerMovement otherPlayer;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        turnWeapon();
+        changeBulletColor();
+        chargeCombination();
+
+        if (Input.GetKeyDown(player.playerFire))
+        {
+            Shoot();
+        }
+
+    }
+
+
+
+    void changeBulletColor()
+    {
+        bulletColor = player.playerColor;
+    }
+    void chargeCombination()
+    {
+        if (Input.GetKeyDown(player.playerCombination))
+        {
+            float timeForCombinationToCharge = 2;
+            while (timeForCombinationToCharge > 0)
+            {
+                timeForCombinationToCharge -= Time.deltaTime;
+
+            }
+            if (timeForCombinationToCharge <= 0)
+            {
+                player.tryingCombination = true;
+                if (otherPlayer.tryingCombination)
+                {
+                    Debug.Log("explosion");
+                    player.tryingCombination = false;
+                    otherPlayer.tryingCombination = false;
+                }
+            }
+            if (Input.GetKeyUp(player.playerCombination))
+            {
+                player.tryingCombination = false;
+                timeForCombinationToCharge = 2;
+            }
+        }
+    }
+    void Shoot()
+    {
+        GameObject projectile = Instantiate(bulletPrefab, firePoint.position, firePoint.rotation);
+        projectile.GetComponent<Bullet>().makeColor(bulletColor);
+    }
+
+
+    void turnWeapon()
+    {
+        if (Input.GetAxisRaw(player.PlayerHorizontal) == 1)
+        {
+            firePoint.rotation = Quaternion.Euler(0f, 0f, 0f);
+        }
+
+        if (Input.GetAxisRaw(player.PlayerHorizontal) == -1)
+        {
+            firePoint.rotation = Quaternion.Euler(0f, 180f, 0f);
+        }
+
+        if (Input.GetAxisRaw(player.PlayerVertical) == 1)
+        {
+            firePoint.rotation = Quaternion.Euler(0f, 0f, 90f);
+        }
+
+        if (Input.GetAxisRaw(player.PlayerVertical) == -1)
+        {
+            firePoint.rotation = Quaternion.Euler(0f, 0f, 270f);
+        }
+    }
+
+}
