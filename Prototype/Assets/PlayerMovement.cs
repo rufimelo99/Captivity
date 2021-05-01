a87b234bec498a603026a85a4585e6a1cd2231e9
Melo <rufimelo99@gmail.com>
2021-05-01 17:38:56 +0100

    banana 27

--- Prototype/Assets/PlayerMovement.cs
+++ Prototype/Assets/PlayerMovement.cs
@@ -4,6 +4,18 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    [SerializeField]
+    private int player;
+
+    public string PlayerColor = "Color";
+    public string PlayerHorizontal = "Horizontal";
+    public string PlayerVertical = "Vertical";
+
+    private KeyCode heroGreen = KeyCode.Q;
+    private KeyCode heroBlue = KeyCode.E;
+    public KeyCode playerFire = KeyCode.Minus;
+    public KeyCode playerCombination = KeyCode.RightShift;
+    public bool tryingCombination = false;
     public float moveSpeed = 5f;
 
     public Rigidbody2D rb;
@@ -15,7 +27,22 @@ public class PlayerMovement : MonoBehaviour
 
     void Start()
     {
+<<<<<<< Updated upstream
         animator.SetInteger("Color", playerColor);
+=======
+        if (player == 2)
+        {
+            PlayerColor = "Color2";
+            PlayerHorizontal = "Horizontal2";
+            PlayerVertical = "Vertical2";
+            heroBlue = KeyCode.K;
+            heroGreen = KeyCode.L; 
+            playerFire = KeyCode.Space; 
+            playerCombination = KeyCode.V;
+        }
+        animator.SetInteger(PlayerColor, playerColor);
+        healthBar.GetComponent<Image>().color = Color.green;
+>>>>>>> Stashed changes
     }
 
     //void OnTriggerEnter2D(Collider2D obj)
@@ -29,8 +56,19 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+<<<<<<< Updated upstream
 
         changeColor();
+=======
+        move();
+        animate();
+        changeColor();
+        if (health <= 0.1)
+        {
+            die();
+        }
+    }
+>>>>>>> Stashed changes
 
         movement.x = Input.GetAxisRaw("Horizontal");
         movement.y = Input.GetAxisRaw("Vertical");
@@ -38,19 +76,39 @@ public class PlayerMovement : MonoBehaviour
         animator.SetFloat("Horizontal", movement.x);
         animator.SetFloat("Vertical", movement.y);
         animator.SetFloat("Speed", movement.sqrMagnitude);
+<<<<<<< Updated upstream
+=======
+        healthBar.fillAmount = health / 10f;
     }
 
+    void move()
+    {
+        movement.x = Input.GetAxisRaw(PlayerHorizontal);
+        movement.y = Input.GetAxisRaw(PlayerVertical);
+>>>>>>> Stashed changes
+    }
 
+    //GOTTA CHANGE HERE
     void changeColor()
     {
+<<<<<<< Updated upstream
         if (Input.GetKeyDown(KeyCode.Alpha0))
+=======
+        
+        if (Input.GetKeyDown(heroGreen))
+>>>>>>> Stashed changes
         {
             playerColor = 0;
         }
+<<<<<<< Updated upstream
         if (Input.GetKeyDown(KeyCode.Alpha1))
+=======
+        if (Input.GetKeyDown(heroBlue))
+>>>>>>> Stashed changes
         {
             playerColor = 1;
         }
+<<<<<<< Updated upstream
 
         animator.SetInteger("Color", playerColor);
     }
@@ -61,9 +119,78 @@ public class PlayerMovement : MonoBehaviour
         rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
         
         if(Input.GetAxisRaw("Horizontal")!= 0 || Input.GetAxisRaw("Vertical") != 0)
+=======
+
+        animator.SetInteger("Color", playerColor);
+    }
+
+    void OnCollisionEnter2D(Collision2D obj)
+    {
+        if (obj.gameObject.tag == "Evil Touch")
+>>>>>>> Stashed changes
         {
-            animator.SetFloat("lastMoveX", Input.GetAxisRaw("Horizontal"));
-            animator.SetFloat("lastMoveY", Input.GetAxisRaw("Vertical"));
+            health = health - 0.1f;
         }
     }
+<<<<<<< Updated upstream
+=======
+
+    void OnCollisionStay2D(Collision2D obj)
+    {
+        if (obj.gameObject.tag == "Evil Touch")
+        {
+            health = health - 0.1f;
+        }
+    }
+
+
+    void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
+        rotate();
+    }
+
+    void rotate()
+    {
+        if (Input.GetAxisRaw(PlayerHorizontal) != 0 || Input.GetAxisRaw(PlayerVertical) != 0)
+        {
+            animator.SetFloat("lastMoveX", movement.x);//Input.GetAxisRaw("Horizontal2")
+            animator.SetFloat("lastMoveY", movement.y);// Input.GetAxisRaw("Vertical2"));
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+
+        if (col.tag == "Obstacle_blue" && playerColor != 1)
+        {
+            health = 0;
+        }
+
+        if (col.tag == "Vertical Room Changer")
+        {
+            roomMover = new Vector2(0, movement.y);
+            gameObject.transform.position = rb.position + roomMover;
+        }
+
+        if (col.tag == "Horizontal Room Changer")
+        {
+            roomMover = new Vector2(movement.x, 0);
+            gameObject.transform.position = rb.position + roomMover;
+        }
+
+        if (col.tag == "Small Enemy Bullet")
+        {
+            health = health - 1;
+            healthBar.fillAmount = health / 10f;
+        }
+
+        if (col.tag == "endGame")
+        {
+            Application.Quit();
+        }
+    }
+
+>>>>>>> Stashed changes
 }
+
--- Prototype/Assets/Weapon.cs
+++ Prototype/Assets/Weapon.cs
@@ -4,21 +4,31 @@ using UnityEngine;
 
 public class Weapon : MonoBehaviour
 {
+    [SerializeField]
+    private PlayerMovement player; 
+    private int bulletColor = 0;
+
 
     public Transform firePoint;
     public GameObject bulletPrefab;
-    public GameObject projectile;
-    public int bulletColor = 0;
+
+    //TODO
+    public PlayerMovement otherPlayer;
 
     // Update is called once per frame
     void Update()
     {
-
+       
         turnWeapon();
         changeBulletColor();
+        chargeCombination();
 
+<<<<<<< Updated upstream
 
         if (Input.GetButtonDown("Fire1"))
+=======
+        if (Input.GetKeyDown(player.playerFire))
+>>>>>>> Stashed changes
         {
             Shoot();
         }
@@ -26,6 +36,7 @@ public class Weapon : MonoBehaviour
 
     void changeBulletColor()
     {
+<<<<<<< Updated upstream
         if (Input.GetKeyDown(KeyCode.Alpha0))
         {
             bulletColor = 0;
@@ -34,33 +45,63 @@ public class Weapon : MonoBehaviour
         {
             bulletColor = 1;
         }
+=======
+         bulletColor = player.playerColor;
+>>>>>>> Stashed changes
     }
 
     void Shoot()
     {
-        projectile = Instantiate(bulletPrefab, firePoint.position, firePoint.rotation);
+        GameObject projectile = Instantiate(bulletPrefab, firePoint.position, firePoint.rotation);
         projectile.GetComponent<Bullet>().makeColor(bulletColor);
     }
 
+    void chargeCombination()
+    {
+        if (Input.GetKeyDown(player.playerCombination))
+        {
+            float timeForCombinationToCharge = 2;
+            while (timeForCombinationToCharge > 0)
+            {
+                timeForCombinationToCharge -= Time.deltaTime;
+                
+            }
+            if (timeForCombinationToCharge <= 0)
+            {
+                player.tryingCombination = true;
+                if (otherPlayer.tryingCombination) 
+                {
+                    Debug.Log("explosion");
+                    player.tryingCombination = false;
+                    otherPlayer.tryingCombination = false;
+                }
+            }
+            if (Input.GetKeyUp(player.playerCombination))
+            {
+                player.tryingCombination = false;
+                timeForCombinationToCharge = 2;
+            }
+        }
+    }
 
     void turnWeapon()
     {
-        if (Input.GetAxisRaw("Horizontal") == 1)
+        if (Input.GetAxisRaw(player.PlayerHorizontal) == 1)
         {
             firePoint.rotation = Quaternion.Euler(0f, 0f, 0f);
         }
 
-        if (Input.GetAxisRaw("Horizontal") == -1)
+        if (Input.GetAxisRaw(player.PlayerHorizontal) == -1)
         {
             firePoint.rotation = Quaternion.Euler(0f, 180f, 0f);
         }
 
-        if (Input.GetAxisRaw("Vertical") == 1)
+        if (Input.GetAxisRaw(player.PlayerVertical) == 1)
         {
             firePoint.rotation = Quaternion.Euler(0f, 0f, 90f);
         }
 
-        if (Input.GetAxisRaw("Vertical") == -1)
+        if (Input.GetAxisRaw(player.PlayerVertical) == -1)
         {
             firePoint.rotation = Quaternion.Euler(0f, 0f, 270f);
         }
