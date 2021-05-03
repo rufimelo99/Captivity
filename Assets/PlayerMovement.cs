ca8e6584916a0f9365816ebf1b4df457880c14b4
Melo <rufimelo99@gmail.com>
2021-05-03 22:48:57 +0100

    pressure plates

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -32,6 +32,21 @@ public class PlayerMovement : MonoBehaviour
     public Image healthBar = null;
 
 
+
+    //Pressure Plate lvl1
+    //Objects on Ground
+    bool door0_opened = false;
+    [SerializeField]
+    private Tilemap tileMap;
+    [SerializeField]
+    private Tilemap doorsToUnlock;
+    [SerializeField]
+    private Tile tilePlateNotActive;
+    [SerializeField]
+    private Tile tilePlateActive;
+
+    //otherPlayer
+    public Rigidbody2D rb2;
     void Start()
     {
         if (player == 2)
@@ -60,6 +75,7 @@ public class PlayerMovement : MonoBehaviour
         changeColor();
         move();
         animate();
+        checkPressurePlates();
         if (health <= 0.1)
         {
             die();
@@ -67,6 +83,79 @@ public class PlayerMovement : MonoBehaviour
         
     }
 
+    void checkPressurePlates()
+    {
+        if (!door0_opened)
+        {
+            bool firstPressurePlateActive = false;
+            bool secondPressurePlateActive = false;
+            Vector3Int playerPosition = new Vector3Int(Mathf.RoundToInt(rb.position.x), Mathf.RoundToInt(rb.position.y), 0);
+            Vector3Int otherPlayerPosition = new Vector3Int(Mathf.RoundToInt(rb2.position.x), Mathf.RoundToInt(rb2.position.y), 0);
+
+            Vector3Int gridPosition = tileMap.WorldToCell(playerPosition);
+            Vector3Int gridPosition2 = tileMap.WorldToCell(otherPlayerPosition);
+
+            Vector3Int firstPressurePlateReal = new Vector3Int(-4, -20, 0);//onTileMap
+            float distanceTofirstPressurePlate = Mathf.Sqrt((rb.position.x - (-4.0f)) * (rb.position.x - (-4.0f)) + (rb.position.y - (-20.0f+1)) * (rb.position.y - (-20.0f + 1)));
+            float distanceTofirstPressurePlate2 = Mathf.Sqrt((rb2.position.x - (-4.0f)) * (rb2.position.x - (-4.0f)) + (rb2.position.y - (-20.0f + 1)) * (rb2.position.y - (-20.0f + 1)));
+
+            if ((distanceTofirstPressurePlate <= 1.0f && rb.position.x > -4 && rb.position.y > -20) ||
+                (distanceTofirstPressurePlate2 <= 1.0f && rb2.position.x > -4 && rb2.position.y > -20))
+            {
+                tileMap.SetTile(firstPressurePlateReal, null);
+                tileMap.SetTile(firstPressurePlateReal, tilePlateActive);
+                firstPressurePlateActive = true;
+            }
+            else
+            {
+                tileMap.SetTile(firstPressurePlateReal, null);
+                tileMap.SetTile(firstPressurePlateReal, tilePlateNotActive);
+
+            }
+
+            Vector3Int secondPressurePlateReal = new Vector3Int(49, -23, 0);//onTileMap
+            float distanceToSecondPressurePlate = Mathf.Sqrt((rb.position.x - (49.0f)) * (rb.position.x - (49.0f)) + (rb.position.y - (-22.0f)) * (rb.position.y - (-22.0f)));
+            float distanceToSecondPressurePlate2 = Mathf.Sqrt((rb2.position.x - (49.0f)) * (rb2.position.x - (49.0f)) + (rb2.position.y - (-22.0f)) * (rb2.position.y - (-22.0f)));
+            if ((distanceToSecondPressurePlate <= 1.0f && rb.position.x > 49 && rb.position.y > -22) ||
+                (distanceToSecondPressurePlate2 <= 1.0f && rb2.position.x > 49 && rb2.position.y > -22))
+            {
+                tileMap.SetTile(secondPressurePlateReal, null);
+                tileMap.SetTile(secondPressurePlateReal, tilePlateActive);
+
+                secondPressurePlateActive = true;
+            }
+            else
+            {
+                tileMap.SetTile(secondPressurePlateReal, null);
+                tileMap.SetTile(secondPressurePlateReal, tilePlateNotActive);
+
+            }
+
+            if(secondPressurePlateActive && firstPressurePlateActive)
+            {
+                Vector3Int wallLocation0_0Doorlayer = new Vector3Int(48, -15, 0);
+                Vector3Int wallLocation0_1Doorlayer = new Vector3Int(48, -14, 0);
+                Vector3Int wallLocation0_2Doorlayer = new Vector3Int(48, -13, 0);
+                Vector3Int wallLocation0_3Doorlayer = new Vector3Int(50, -13, 0);
+                Vector3Int wallLocation0_4Doorlayer = new Vector3Int(50, -14, 0);
+                Vector3Int wallLocation0_5Doorlayer = new Vector3Int(50, -15, 0);
+                Vector3Int wallLocation0_6Doorlayer = new Vector3Int(49, -13, 0);
+                Vector3Int wallLocation0_7Doorlayer = new Vector3Int(49, -14, 0);
+                Vector3Int wallLocation0_8Doorlayer = new Vector3Int(49, -15, 0);
+                doorsToUnlock.SetTile(wallLocation0_0Doorlayer, null);
+                doorsToUnlock.SetTile(wallLocation0_1Doorlayer, null);
+                doorsToUnlock.SetTile(wallLocation0_2Doorlayer, null);
+                doorsToUnlock.SetTile(wallLocation0_3Doorlayer, null);
+                doorsToUnlock.SetTile(wallLocation0_4Doorlayer, null);
+                doorsToUnlock.SetTile(wallLocation0_5Doorlayer, null);
+                doorsToUnlock.SetTile(wallLocation0_6Doorlayer, null);
+                doorsToUnlock.SetTile(wallLocation0_7Doorlayer, null);
+                doorsToUnlock.SetTile(wallLocation0_8Doorlayer, null);
+                door0_opened = true;
+            }
+
+        }
+    }
 
     void die()
     {
