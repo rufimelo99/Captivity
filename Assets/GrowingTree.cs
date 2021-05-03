270207bd220941984159fe3e4d02d0c6e4e1b5ba
miguellahera <36444402+miguellahera@users.noreply.github.com>
2021-05-03 11:40:29 +0100

    last version

---
+++ Assets/GrowingTree.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GrowingTree : MonoBehaviour
+{
+    
+    [SerializeField] private Animator animiate1;
+    [SerializeField] private Collider2D myCollider;
+    
+    
+    void OnCollisionEnter2D(Collision2D obj)
+    {
+        if (obj.gameObject.tag == "Hole")
+        {
+            animiate1.SetBool("Hole", true);
+            myCollider.enabled = false;
+        }
+        
+    }
+}
