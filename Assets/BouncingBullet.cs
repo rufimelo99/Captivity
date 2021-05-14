2a9ac5c964a684a353ab119aafe74faedf737ff1
eskapinakis <eduardoskapinakis1234@gmail.com>
2021-05-14 15:35:57 +0000

    dumb bouncing bullet

---
+++ Assets/BouncingBullet.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BouncingBullet : MonoBehaviour
+{
+
+    public Rigidbody2D rb;
+    public float speed = 20f;
+    private Vector2 right;
+    private Vector2 left;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        right = new Vector2(-1f, 0f);
+        left = new Vector2(1f, 0f);
+
+        rb.velocity = right * speed;
+    }
+
+    void OnCollisionEnter2D(Collision2D obj)
+    {
+        rb.velocity = -1f * rb.velocity;
+    }
+}
