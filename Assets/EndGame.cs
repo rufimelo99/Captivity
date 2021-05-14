a1a0aea7c42e953358b044a4bc496514a34563ec
eskapinakis <eduardoskapinakis1234@gmail.com>
2021-05-14 09:53:53 +0000

    elementals noice

--- Assets/EndGame.cs
+++ Assets/EndGame.cs
@@ -5,19 +5,51 @@ using UnityEngine.SceneManagement;
 
 public class EndGame : MonoBehaviour
 {
+
+    public int level;
+    public int element;
+
     void OnTriggerEnter2D(Collider2D obj)
     {
         if (obj.tag == "Player" || obj.tag == "Player2")
         {
             // SceneManager.LoadScene(0);
 
-            GlobalControl.Instance.elementalsPossesed.Add(Player.ElementalsAvailable.WATER);
-            GlobalControl.Instance.CompletedLevels = 1;
+            giveElemental();
+            GlobalControl.Instance.CompletedLevels = level;
             //GlobalControl.Instance.player2.elementalsPossesed.Add(Player.ElementalsAvailable.WATER);
 
             SceneManager.LoadScene(3);
 
         }
+    }
 
+
+    void giveElemental()
+    {
+        if (element == 1)
+        {
+            GlobalControl.Instance.elementalsPossesed.Add(Player.ElementalsAvailable.WATER);
+        }
+        if (element == 2)
+        {
+            GlobalControl.Instance.elementalsPossesed.Add(Player.ElementalsAvailable.GROUND);
+        }
+        if (element == 3)
+        {
+            GlobalControl.Instance.elementalsPossesed.Add(Player.ElementalsAvailable.FIRE);
+        }
+        if (element == 4)
+        {
+            GlobalControl.Instance.elementalsPossesed.Add(Player.ElementalsAvailable.AIR);
+        }
+        if (element == 5)
+        {
+            GlobalControl.Instance.elementalsPossesed.Add(Player.ElementalsAvailable.ELECTRICITY);
+        }
+        else
+        {
+            ;
+        }
     }
 }
