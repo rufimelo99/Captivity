2a6d8ec59f1ed60db86b7faecabf959918189ab8
eskapinakis <eduardoskapinakis1234@gmail.com>
2021-05-15 22:21:13 +0000

    navigation but color is weird

---
+++ Assets/ShowButtons.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShowButtons : MonoBehaviour
+{
+
+    [SerializeField]
+    private GameObject resume_button;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        if (GlobalControl.Instance.CompletedLevels == 0) { 
+            Destroy(resume_button);
+        }
+    }
+
+}
