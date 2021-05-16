cdc4c91185f5e21a467d202f55fe6bfeeb2658f5
eskapinakis <eduardoskapinakis1234@gmail.com>
2021-05-16 12:23:19 +0000

    disclaimer

--- Assets/ShowButtons.cs
+++ Assets/ShowButtons.cs
@@ -7,14 +7,28 @@ public class ShowButtons : MonoBehaviour
 
     [SerializeField]
     private GameObject resume_button;
+    [SerializeField]
+    private bool menu; //this is cause I is slightly different in the menu and in the controls scene and i didn't wannna make 2 scripts
 
     // Start is called before the first frame update
     void Start()
     {
+        if (menu) {
 
-        if (GlobalControl.Instance.CompletedLevels == 0) { 
-            Destroy(resume_button);
+            if (GlobalControl.Instance.CompletedLevels == 0) //to destroy the resume button
+            {
+                Destroy(resume_button);
+            }
         }
+        else
+        {
+            if (GlobalControl.Instance.CompletedLevels >= 2) //to destroy the "you have no powers" button
+            {
+                Destroy(resume_button);
+            }
+        }
+
+        
     }
 
 }
