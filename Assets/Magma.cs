15383e4755394ce5b8559e2b594a145ab5c37c9a
eskapinakis <eduardoskapinakis1234@gmail.com>
2021-05-20 18:08:24 +0000

    mamamamnaavwtdhvaw wa

---
+++ Assets/Magma.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Magma : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(freeze());
+    }
+
+    IEnumerator freeze()
+    {
+        yield return new WaitForSeconds(10f);
+        Destroy(gameObject);
+    }
+}
