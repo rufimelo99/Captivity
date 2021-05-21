4de0d067306dc5399c5b9fa571644367d06b5bbb
Melo <rufimelo99@gmail.com>
2021-05-21 02:42:15 +0100

    inicial notes on 1st level

---
+++ Assets/PaperPiecesTrigger.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PaperPiecesTrigger : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        DisplayDialogue displayDialogue = gameObject.GetComponent<DisplayDialogue>();
+        if (displayDialogue)
+        {
+            displayDialogue.displayDialogue();
+        }
+        Destroy(gameObject);
+    }
+}
