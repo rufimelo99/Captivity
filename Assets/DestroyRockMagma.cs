5485710609db0797c92782e7f2639c71dca40677
miguellahera <36444402+miguellahera@users.noreply.github.com>
2021-05-27 22:38:49 +0100

    last update

---
+++ Assets/DestroyRockMagma.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyRockMagma : MonoBehaviour
+{
+
+    void OnTriggerEnter2D(Collider2D obj)
+    {
+        if (obj.tag == "Magma")
+        {
+            Destroy(gameObject);
+        }
+    }
+
+}
