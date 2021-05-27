4d14e9059ed808b52fd024f3a1bececa45129672
miguellahera <36444402+miguellahera@users.noreply.github.com>
2021-05-27 01:10:53 +0100

    holaaa

--- Assets/PipeTeletransportation.cs
+++ Assets/PipeTeletransportation.cs
@@ -4,6 +4,18 @@ using UnityEngine;
 
 public class PipeTeletransportation : MonoBehaviour
 {
+    private float distanceToVapourPrefab;
+    [SerializeField] public GameObject fusionVapourPrefab;
+
+    [SerializeField] public GameObject pipe;
+    [SerializeField] public GameObject roomChangerToMistery;
+    [SerializeField] private Collider2D myCollider;
+    [SerializeField] private SpriteRenderer rend;
+    [SerializeField] private Animator tornado;
+    [SerializeField] private Collider2D myColliderTornado;
+    [SerializeField] private SpriteRenderer rendTornado;
+
+    
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +25,9 @@ public class PipeTeletransportation : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        distanceToVapourPrefab = (pipe.transform.position - tornado.transform.position).sqrMagnitude;
+        if(distanceToVapourPrefab < 3 && tornado.transform.position == roomChangerToMistery.transform.position){
+            roomChangerToMistery.transform.position = roomChangerToMistery.transform.position+new Vector3(0,0,1f);
+        }
     }
 }
