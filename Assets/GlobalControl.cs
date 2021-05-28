892738b36eeef728f89a303dcac49dd52fe6fb3d
miguellahera <36444402+miguellahera@users.noreply.github.com>
2021-05-28 19:21:50 +0100

    last update

--- Assets/GlobalControl.cs
+++ Assets/GlobalControl.cs
@@ -74,15 +74,15 @@ public class GlobalControl : MonoBehaviour
         gemsCollected.Add(GlobalControl.GemsAvailable.WIZARD);
         */
 
-        /*FOR TESTS - DO NOT FORGET TO COMMENT FOR THE BUILD 
-        elementalsPossesed.Add(Player.ElementalsAvailable.WATER);
+        //FOR TESTS - DO NOT FORGET TO COMMENT FOR THE BUILD 
+        /*elementalsPossesed.Add(Player.ElementalsAvailable.WATER);
         elementalsPossesed.Add(Player.ElementalsAvailable.GROUND);
         elementalsPossesed.Add(Player.ElementalsAvailable.FIRE);
         elementalsPossesed.Add(Player.ElementalsAvailable.AIR);
         elementalsPossesed.Add(Player.ElementalsAvailable.ELECTRICITY);
-        pagesCollected = new bool[16]; //needs to be equal to the number of pages on the book
-        #To allow all the pages on the diary
-        for (int i=0; i<pagesCollected.Length; i++)
+        pagesCollected = new bool[16]; //needs to be equal to the number of pages on the book*/
+        //#To allow all the pages on the diary
+        /*for (int i=0; i<pagesCollected.Length; i++)
         {
             pagesCollected[i] = true;
         }*/
