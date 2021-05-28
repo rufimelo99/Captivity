0187f1baffcb95aa295952e79b39b3fddb5da8e4
eskapinakis <eduardoskapinakis1234@gmail.com>
2021-05-28 13:34:18 +0000

    beatiful

--- Assets/EvilWizard.cs
+++ Assets/EvilWizard.cs
@@ -6,9 +6,18 @@ public class EvilWizard : MonoBehaviour
 {
 
     private bool left = true;
+    private Vector3 offset = new Vector3(-3f, 0f, 0f);
     private float health = 10f;
 
     public Animator animator;
+
+    public GameObject player1;
+    public GameObject player2;
+
+
+    public GameObject molePrefab;
+    public GameObject EvilTreePrefab;
+    public GameObject WaterBossPrefab;
 
 
     // Start is called before the first frame update
@@ -26,10 +35,12 @@ public class EvilWizard : MonoBehaviour
         if (left)
         {
             transform.rotation = Quaternion.Euler(0, 180, 0);
+            offset = new Vector3(-3f, 0f, 0f);
         }
         else
         {
             transform.rotation = Quaternion.Euler(0, 0, 0);
+            offset = new Vector3(3f, 0f, 0f);
         }
     }
 
@@ -38,11 +49,21 @@ public class EvilWizard : MonoBehaviour
     {
         while (true)
         {
-            yield return new WaitForSeconds(5.0f);
+            yield return new WaitForSeconds(2.0f);
             animator.SetBool("Attack", true);
+            manageAttacks();
             yield return new WaitForSeconds(0.1f);
             animator.SetBool("Attack", false);
         }
+    }
+
+
+    void manageAttacks()
+    {
+        //Instantiate(EvilTreePrefab, transform.position + offset, transform.rotation);
+        GameObject moly = Instantiate(WaterBossPrefab, transform.position + offset, transform.rotation*Quaternion.Euler(0, 180, 0));
+        moly.SetActive(true);
+        moly.GetComponent<WizardMole>().addPlayers(player1, player2);
     }
 
 
---
+++ Assets/WizardMole.cs
@@ -0,0 +1,178 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class WizardMole : MonoBehaviour
+{
+    public float speed;
+
+    private bool frozen = false;
+    private bool destroyed = false;
+
+    private float startTime;
+    private bool isIn = true;
+
+    public Animator animator;
+
+
+    public Transform Player1;
+    public Transform Player2;
+    private Transform closestPlayer;
+
+    private float distance1;
+    private float distance2;
+    private Vector3 direction;
+
+    public GameObject bulletPrefab;
+
+    public float health = 10f;
+    public Image healthBar;
+
+    public int shoot;
+
+    private bool iHaveThemPLayers = false;
+
+
+    void Start()
+    {
+        
+    }
+
+
+    public void addPlayers(GameObject p1, GameObject p2)
+    {
+        Player1 = p1.transform;
+        Player2 = p2.transform;
+        iHaveThemPLayers = true;
+
+        animator.SetFloat("Health", 10);
+        animator.SetFloat("Speed", 0);
+        speed = 3.0f;
+        StartCoroutine(ShotTimer());
+    }
+
+
+    IEnumerator ShotTimer()
+    {
+        WaitForSeconds pause = new WaitForSeconds(1f);
+        while (true)
+        {
+            yield return pause;
+            if (!frozen & iHaveThemPLayers)
+            {
+                Shoot();
+            }
+        }
+    }
+
+    void Shoot()
+    {
+        if (!iHaveThemPLayers)
+        {
+            return;
+        }
+        if (health >= 5)
+        {
+            GameObject bull = Instantiate(bulletPrefab, transform.position, transform.rotation); // follow the player
+            bull.GetComponent<FollowingBullet>().addPlayer(closestPlayer);
+        }
+        else
+        {
+            GameObject bull1 = Instantiate(bulletPrefab, transform.position, transform.rotation); // follow the player
+            bull1.GetComponent<FollowingBullet>().addPlayer(Player1);
+            GameObject bull2 = Instantiate(bulletPrefab, transform.position, transform.rotation); // follow the player
+            bull2.GetComponent<FollowingBullet>().addPlayer(Player2);
+        }
+
+
+    }
+
+    void OnTriggerEnter2D(Collider2D obj)
+    {
+        if (obj.tag == "Bullet")
+        {
+            health = health - obj.GetComponent<Bullet>().damage;
+        }
+        healthBar.fillAmount = health / 10f;
+        animator.SetFloat("Health", health);
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!iHaveThemPLayers)
+        {
+            return;
+        }
+
+        distance1 = (Player1.position - transform.position).sqrMagnitude;
+        distance2 = (Player2.position - transform.position).sqrMagnitude;
+
+        if (distance1 <= distance2)
+        {
+            direction = Player1.position - transform.position;
+            closestPlayer = Player1;
+        }
+        else
+        {
+            direction = Player2.position - transform.position;
+            closestPlayer = Player2;
+        }
+
+        animate();
+
+        if (health <= 0)
+        {
+            destroyed = true;
+            StartCoroutine(freeze());
+        }
+
+    }
+
+    Quaternion GetRotationTo(Transform target)
+    {
+        Vector3 dir = target.position - transform.position;
+        float angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
+        return Quaternion.AngleAxis(angle, Vector3.forward);
+    }
+
+    void animate()
+    {
+        animator.SetFloat("Horizontal2", direction.x);
+        animator.SetFloat("Vertical2", direction.y);
+    }
+
+
+    void FixedUpdate()
+    {
+
+        if (!iHaveThemPLayers)
+        {
+            return;
+        }
+
+        animator.SetFloat("Speed", speed);
+        if (distance1 <= distance2)
+        {
+            transform.position = Vector2.MoveTowards(transform.position, Player1.position, Time.deltaTime * speed);
+        }
+        else
+        {
+            transform.position = Vector2.MoveTowards(transform.position, Player2.position, Time.deltaTime * speed);
+        }
+
+    }
+
+    
+
+    IEnumerator freeze()
+    {
+        frozen = true;
+        speed = 0f;
+        yield return new WaitForSeconds(1f);
+        frozen = false;
+        speed = 2f;
+    }
+}
